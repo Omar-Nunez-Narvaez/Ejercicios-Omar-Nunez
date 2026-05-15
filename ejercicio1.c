39e31f8684007a6ecef9684357a8ae4036baa048
Omar-Nunez-Narvaez <omar.nunez@udla.edu.ec>
2026-05-15 14:48:15 -0500

    Tercer y ultimo commit del Primer Ejercicio Omar Nuñez

--- ejercicio1.c
+++ ejercicio1.c
@@ -2,10 +2,12 @@
 
 int main() {
     int arreglo[] = {3, 6, 9, 12, 15, 18};
-    int *ptr = arreglo;  
+    int *ptr;
+    ptr = &arreglo [0];  // ptr = arreglo;  
 
     for (int i = 0; i < 6; i++) {
-        printf("%d ", *(ptr + i));  
+        printf("arrglo[%d] = %d ", i, arreglo[i]);
+        printf("%d\ngit add .", *(ptr + i));  
     }
 
     printf("\n");
