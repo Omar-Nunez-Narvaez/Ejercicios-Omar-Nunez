819b4e003311d837f58edac352908e6b0df75b76
Omar-Nunez-Narvaez <omar.nunez@udla.edu.ec>
2026-05-15 15:23:53 -0500

    Tercer commit del ejercicio 3 Omar Nuñez

--- ejercicio3.c
+++ ejercicio3.c
@@ -2,13 +2,14 @@
 
 int main() {
     char vocales[] = {'a', 'e', 'i', 'o', 'u'};
-    char *ptr = vocales;  
-
-  
-    for (int i = 0; i < 5; i++) {
-        printf("%c ", *(ptr + i));  
-    }
+    char *ptr;
+    ptr = &vocales[3];  *ptr = 'x';
 
     
+    for (int i = 0; i < 5; i++) {
+        printf("%c ", vocales [i]);  
+    }
+
+
     return 0;
-}
+}git add
