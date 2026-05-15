e5d5a9f25e5da042e726d48407680088fb1d4959
Omar-Nunez-Narvaez <omar.nunez@udla.edu.ec>
2026-05-15 15:16:48 -0500

    Agregando un vector de vocales Ejercicio3 commit 1 Omar Nunez

--- ejercicio3.c
+++ ejercicio3.c
@@ -1,1 +1,12 @@
-#include <stdio.h> 
+#include <stdio.h>
+
+int main() {
+    char vocales[] = {'a', 'e', 'i', 'o', 'u'};
+
+    for (int i = 0; i < 5; i++) {
+        printf("%c ", vocales[i]);
+    }
+
+    printf("\n");
+    return 0;
+}
