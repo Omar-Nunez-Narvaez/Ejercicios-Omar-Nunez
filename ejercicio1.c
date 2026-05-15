a6d50f8bfdc1e2044f2f8e3c5df875a4fcd6294b
Omar-Nunez-Narvaez <omar.nunez@udla.edu.ec>
2026-05-15 14:56:38 -0500

    Tecer commit del segundo ejercicio de Cisco Omar Nuñez

--- ejercicio1.c
+++ ejercicio1.c
@@ -2,14 +2,17 @@
 
 int main() {
     int arreglo[] = {3, 6, 9, 12, 15, 18};
-    int *ptr;
-    ptr = &arreglo [0];  // ptr = arreglo;  
+    int j = sizeof arreglo;
+    printf("%d byte(s)\n", j);
 
-    for (int i = 0; i < 6; i++) {
-        printf("arrglo[%d] = %d ", i, arreglo[i]);
-        printf("%d\ngit add .", *(ptr + i));  
-    }
-
-    printf("\n");
+    printf("This computing environment uses:\n");
+    printf("%zu byte for chars\n", sizeof(char));
+    printf("%zu bytes for shorts\n", sizeof(short int));
+    printf("%zu bytes for ints\n", sizeof(int));
+    printf("%zu bytes for longs\n", sizeof(long int));
+    printf("%zu bytes for long longs\n", sizeof(long long int));
+    printf("%zu bytes for floats\n", sizeof(float));
+    printf("%zu bytes for doubles\n", sizeof(double));
+    printf("%zu bytes for pointers\n", sizeof(int *));
     return 0;
 }
