a8cf205bff89b3999f6c49595fcc9494acac2790
Omar-Nunez-Narvaez <omar.nunez@udla.edu.ec>
2026-05-15 14:44:58 -0500

    Segudo commit: Omar Nunez añadiendo un puntero

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,12 +1,13 @@
-
 #include <stdio.h>
 
 int main() {
     int arreglo[] = {3, 6, 9, 12, 15, 18};
-    for (int i = 0; i < 5; i++) {
-        printf("%d ", arreglo[i]);
+    int *ptr = arreglo;  
+
+    for (int i = 0; i < 6; i++) {
+        printf("%d ", *(ptr + i));  
     }
+
+    printf("\n");
     return 0;
-
-
 }
